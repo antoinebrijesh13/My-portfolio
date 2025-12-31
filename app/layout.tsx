0bf08ca12dc32fced8553618ffe10e155e0a2101
Antony Brijesh <antony.brijesh@btech.christuniversity.in>
2025-12-31 13:00:39 +0530

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Archivo_Black, Oswald } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from './context/ThemeContext'
 import Cursor from './components/Cursor'
+import GoogleAnalytics from './components/GoogleAnalytics'
 import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
@@ -30,6 +31,7 @@ export default function RootLayout({
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700,400&display=swap" rel="stylesheet" />
       </head>
       <body className={`${inter.variable} ${montserrat.variable} ${archivoBlack.variable} ${oswald.variable} font-sans antialiased overflow-x-hidden`}>
+        <GoogleAnalytics />
         <ThemeProvider>
           {/* Only render Cursor if not on /photography or its subpages */}
           {!(pathname.startsWith('/photography')) && <Cursor />}
